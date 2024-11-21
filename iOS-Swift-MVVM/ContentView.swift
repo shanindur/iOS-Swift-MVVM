8f4a10ba6a0da2ef65e023c3bcbe3a7df921c5b4
shanindurajapaksha <shanindu.rajapaksha91@gmail.com>
2024-11-21 15:20:21 -0500

    Initial commit

--- iOS-Swift-MVVM/ContentView.swift
+++ iOS-Swift-MVVM/ContentView.swift
@@ -14,6 +14,7 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, Shanindu!")
+            Text("This is my frst app.")
         }
         .padding()
     }
